94e9e49985e1c0f8019e23e4d93b50fbb4a312ce
romeolandry <kamgoche@yahoo.com>
2019-09-23 17:31:06 +0200

    Train-Funktion

--- LinearModel.py
+++ LinearModel.py
@@ -15,13 +15,29 @@ class LinearModel:
     def linear_regresion_model(self):
         return (tf.add(tf.multiply(self.__eingabe,self.__gewicht,),self.__bias)) ## model = x*w + b
 
-    def loss_funtion(self, output, model):
-        cost = tf.square(output - model)
+    def loss_funtion(self, Y):
+        cost = tf.square(Y - self.linear_regresion_model)
         train_op = tf.train.GradientDescentOptimizer(self.__lernrate).minimize(cost)
         return train_op
 
-    def anwendung_model (self):
-        with tf.Session() as sess_pred:
-           predicted_output =  sess_pred.run(self.linear_regresion_model())
-           
+    def train_model(self, epoch,input_daten,output_daten, out_funktion,input_model,output_model):
+        with tf.Session() as sess_train:
+            for i in range(epoch):
+                print("----------Epoch: {} ---".format(i))
+                for (x,y) in zip(input_daten,output_daten):
+                    sess_train.run(self.loss_funtion(out_funktion),feed_dict={input_model:x,output_model:y})
+
+                # Berechnung von Gewichtungen und Bias
+                weight_value = sess_train.run(self.__gewicht)
+                bias_value = sess_train.run(self.__bias)
+
+                print("bias_value :{}".format(bias_value))
+                print("weight_value:{}".format(weight_value))
+
+            return (weight_value,bias_value)
+
+    def anwendung_model (self,test_input,weight_value,bias_value):
+        predict_model = tf.add(tf.multiply(test_input,weight_value),bias_value)
+        with tf.Session() as sess_pred:            
+            predicted_output = sess_pred.run(predict_model)
         return predicted_output 
