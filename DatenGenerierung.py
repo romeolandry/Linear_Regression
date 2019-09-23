5f0e67954770e2d6b4d9e32bf0bb5570d4d4d8ee
romeolandry <kamgoche@yahoo.com>
2019-09-23 16:14:44 +0200

    generierung und vimsualisierung der Daten

---
+++ DatenGenerierung.py
@@ -0,0 +1,39 @@
+import tensorflow as tf 
+import numpy as np 
+import matplotlib
+matplotlib.use('TkAgg') # ?
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+class DatenGenerierung:
+    def __init__(self,my_weight,range_input):
+        self.__weight = my_weight
+        self.__input = np.arange(range_input)
+        self.__noise = np.random.randint(low=-5, high=5, size=self.__input)
+
+    #getter und setter
+    def get_weight(self):
+        return self.__weight
+    def set_weight(self,my_weight):
+        self.__weight = my_weight
+
+    def get_input(self):
+        return self.__input
+    def set_input(self,input_value):
+        self.__input = np.arange(input_value)
+    
+    def get_noise(self):
+        return self.__noise
+    def set_noise(self,noise):
+        self.__noise = noise
+    
+    #Daten Generieren
+
+    def gen_daten(self):
+        output = self.__weight * self.__input + self.__noise
+        return (self.__input,output)
+        
+    def data_visualisation(self, input, output):
+        plt.scatter(input, output, c= "red")
+        plt.show()
